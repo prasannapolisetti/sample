379abd23882544514ef58fcf60511ddcb2994c95
Prasanna Polisetti <ppolisetti@corp.miraclesoft.com>
2022-06-04 16:34:08 +0530

    first commit

---
+++ src/main/java/com/springecommerceapplication/Gitdemo1Application.java
@@ -0,0 +1,14 @@
+package com.springecommerceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitdemo1Application.class, args);
+		System.out.println("hai");
+	}
+
+}
